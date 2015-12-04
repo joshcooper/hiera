793672066a38d9d9af487a4ad7363aabe444282c
Josh Cooper <josh@puppetlabs.com>
2015-12-03 17:28:55 -0800

    Spike using beaker-hostgenerator
    Previously, hiera acceptance relied on host definitions in the repo,
    and used two different methods for installing puppet-agent. For redhat
    and debian, we installed the repo-configs in `install_repos_on` and
    installed the packages in `install_packages_on`. For other platforms,
    the call to `install_repos_on` would install puppet-agent.
    
    Adding a new platform required adding a host definition file, and
    sometimes updating the setup steps for install puppet-agent on that
    platform.
    
    This commit modifies the Rakefile to use beaker-hostgenerator to
    generate the host definition from an environment variable `PLATFORMS`,
    and updates the setup step to call beaker's
    `install_puppet_agent_dev_repo` in all cases.
    
    We leave `install_utils` intact, because it is used for `package` style
    acceptance. We could probably remove support for that as well.

--- acceptance/lib/puppet/acceptance/install_utils.rb
+++ acceptance/lib/puppet/acceptance/install_utils.rb
@@ -54,66 +54,6 @@ module Puppet
         return true
       end
 
-      def fetch(base_url, file_name, dst_dir)
-        FileUtils.makedirs(dst_dir)
-        src = "#{base_url}/#{file_name}"
-        dst = File.join(dst_dir, file_name)
-        if File.exists?(dst)
-          logger.notify "Already fetched #{dst}"
-        else
-          logger.notify "Fetching: #{src}"
-          logger.notify "  and saving to #{dst}"
-          open(src) do |remote|
-            File.open(dst, "w") do |file|
-              FileUtils.copy_stream(remote, file)
-            end
-          end
-        end
-        return dst
-      end
-
-      def fetch_remote_dir(url, dst_dir)
-        logger.notify "fetch_remote_dir (url: #{url}, dst_dir #{dst_dir})"
-        if url[-1, 1] !~ /\//
-          url += '/'
-        end
-        url = URI.parse(url)
-        chunks = url.path.split('/')
-        dst = File.join(dst_dir, chunks.last)
-        #determine directory structure to cut
-        #only want to keep the last directory, thus cut total number of dirs - 2 (hostname + last dir name)
-        cut = chunks.length - 2
-        wget_command = "wget -nv -P #{dst_dir} --reject \"index.html*\",\"*.gif\" --cut-dirs=#{cut} -np -nH --no-check-certificate -r #{url}"
-
-        logger.notify "Fetching remote directory: #{url}"
-        logger.notify "  and saving to #{dst}"
-        logger.notify "  using command: #{wget_command}"
-
-        #in ruby 1.9+ we can upgrade this to popen3 to gain access to the subprocess pid
-        result = `#{wget_command} 2>&1`
-        result.each_line do |line|
-          logger.debug(line)
-        end
-        if $?.to_i != 0
-          raise "Failed to fetch_remote_dir '#{url}' (exit code #{$?}"
-        end
-        dst
-      end
-
-      def stop_firewall_on(host)
-        case host['platform']
-        when /debian/
-          on host, 'iptables -F'
-        when /fedora|el-7/
-          on host, puppet('resource', 'service', 'firewalld', 'ensure=stopped')
-        when /el|centos/
-          on host, puppet('resource', 'service', 'iptables', 'ensure=stopped')
-        when /ubuntu/
-          on host, puppet('resource', 'service', 'ufw', 'ensure=stopped')
-        else
-          logger.notify("Not sure how to clear firewall on #{host['platform']}")
-        end
-      end
 
       def install_repos_on(host, project, sha, repo_configs_dir)
         platform = host['platform'].with_version_codename
